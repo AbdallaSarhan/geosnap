21ce2c9531b16c91f223174906d1065e1e499ead
Abdalla Sarhan <abdalla1@my.yorku.ca>
2024-06-29 22:57:18 -0400

    Render markers on the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,15 +1,41 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import Mapbox, { Camera, LocationPuck, MapView } from "@rnmapbox/maps";
+import Mapbox, {
+	Camera,
+	Images,
+	LocationPuck,
+	MapView,
+	ShapeSource,
+	SymbolLayer,
+} from "@rnmapbox/maps";
+import { featureCollection, point } from "@turf/helpers";
+
+import pin from "../assets/images/pin.png";
+
+import pointData from "../data/points.json";
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || "");
 
 const Map = () => {
+	const points = pointData.map((p) => point([p.long, p.lat]));
+	const imageFeatures = featureCollection(points);
 	return (
 		<View style={styles.container}>
 			<MapView style={{ flex: 1 }}>
-				<Camera followUserLocation followZoomLevel={13} />
+				<Camera followUserLocation followZoomLevel={10} />
 				<LocationPuck pulsing={{ isEnabled: true }} />
+
+				<ShapeSource id="images" shape={imageFeatures}>
+					<SymbolLayer
+						id="image-icons"
+						style={{
+							iconImage: "pin",
+							iconSize: 0.1,
+							iconAllowOverlap: true,
+						}}
+					/>
+					<Images images={{ pin }} />
+				</ShapeSource>
 			</MapView>
 		</View>
 	);
